b00c38780f974e64f179be0ef76601a79472b150
Rodrigo Alfonso <rodri042@gmail.com>
2017-01-27 05:19:13 -0300

    Adding rungs wrapper

---
+++ bin/rungs.js
@@ -0,0 +1,23 @@
+// Wrapper of gs-weblang-cli --ast --from_stdin
+// It can be compiled with `nexe`: nexe -i rungs.js -o rungs
+
+var gbs = require('gs-weblang-core/umd/index.umd.min');
+var reporter = require('../src/reporter');
+
+function withCode(action) {
+  var code = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+      code += chunk;
+    }
+  });
+  process.stdin.on('end', () => {
+    action(code);
+  });
+}
+
+withCode(code => {
+  console.log(reporter.getAst(code));
+})
