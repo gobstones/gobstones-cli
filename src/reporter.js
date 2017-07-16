581364b9f4d6d047a686f6c8f215155b27b0cbf6
Franco Bulgarelli <franco@mumuki.org>
2017-07-16 13:20:42 -0300

    Introducing mulang module, extracted from https://github.com/mumuki/mulang/blob/v2.2.0/src/Language/Mulang/Parsers/Gobstones.hs

--- src/reporter.js
+++ src/reporter.js
@@ -1,34 +1,21 @@
 var gsWeblangCore = require("gs-weblang-core/umd/index.umd");
+var Mulang = require('./mulang');
 var Context = gsWeblangCore.Context;
 var parser = gsWeblangCore.getParser();
 var astReplacer = require("./ast-replacer");
 var _ = require("lodash");
 var reporter = {}
 
-function getAstAs(transform) {
+function getJsonAstUsing(stringifier) {
   return function(code) {
     var ast = this._compile(code);
     var nodes = (ast.program ? [ast.program] : []).concat(ast.declarations);
-    return JSON.stringify(transform(nodes), astReplacer, 2);
+    return stringifier(nodes);
   }
 };
 
-function mulangTransformer(program) {
-  return t(program[0]);
-}
-
-function t(s) {
-  if (s.alias == "program") {
-    return { tag: "EntryPoint", contents: ["program", t(s.body[0])]}
-  }
-  if (s.alias == "Drop") {
-    return { tag: "Application", contents: [{tag: "Reference", contents: "Poner"}].concat(s.parameters.map(function(x){return t(x)})) }
-  }
-  return {tag: "Other"}
-}
-
-reporter.getAst = getAstAs(function(x){return x});
-reporter.getMulangAst = getAstAs(mulangTransformer);
+reporter.getAst = getJsonAstUsing((nodes) => JSON.stringify(nodes, astReplacer, 2));
+reporter.getMulangAst = getJsonAstUsing((nodes) => JSON.stringify(Mulang.parse(nodes)));
 
 reporter.run = function(code, initialBoard, format) {
   var ast = this._compile(code).program;
